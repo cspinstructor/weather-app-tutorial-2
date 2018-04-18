06f74631cb42d3ac5084fbcf9eb14eecd1ac0126
paulchin <paulchin20@yahoo.com>
2018-04-18 18:03:02 +0800

    initial commit

---
+++ app.js
@@ -0,0 +1,27 @@
+
+const yargs = require('yargs');
+const axios = require('axios');
+
+const argv = yargs
+.options('address')
+.argv;
+
+const addr = argv.address;
+const locationReq = `https://maps.googleapis.com/maps/api/geocode/json?address=${addr}&key=AIzaSyAn7h3tsW_p0md5iISNFzLcJDoRGRgjWPg`;
+const weatherReq = `https://api.darksky.net/forecast/a8657815b96e87afa1e337d1fb2329d8/37.8267,-122.4233`;
+
+axios.get(locationReq).then((response) => {
+  if (response.data.status === 'ZERO_RESULTS'){
+    console.log('Unable to find that address');
+  } else {
+    const addr = response.data.results[0].formatted_address;
+    const lat = response.data.results[0].geometry.location.lat;
+    const lng = response.data.results[0].geometry.location.lng;
+    return axios.get(weatherReq);
+  }
+}).then((response) => {
+  console.log(response.data.currently.summary);
+})
+.catch((error) => {
+  console.log(error.code);
+});
