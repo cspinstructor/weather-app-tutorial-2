dd40ad33fb9b12750d08b14e7d1661074ab49932
paulchin <paulchin20@yahoo.com>
2018-04-28 22:56:52 +0800

    Added hbs partials

--- app-server-hbs.js
+++ app-server-hbs.js
@@ -21,6 +21,7 @@ server.use(bodyParser.urlencoded({ extended: true}));
 
 server.use(express.static(__dirname + '/public'));
 server.set('view engine', 'hbs');
+hbs.registerPartials(__dirname + '/views/partials');
 
 server.get('/', (req, res) => {
   console.log('render home.hbs');
@@ -29,10 +30,18 @@ server.get('/', (req, res) => {
   //console.log(addr);
 });
 
+server.get('/home', (req, res) => {
+  res.render('home.hbs');
+});
+
+server.get('/form', (req, res) => {
+  res.render('form.hbs');
+});
+
 server.post('/form', (req,res) => {
   console.log('from button Get-Started rendering form2.hbs');
   //res.redirect('/');
-  res.render('form2.hbs');
+  res.render('form.hbs');
 });
 
 server.post('/gohome', (req, res) => {
@@ -57,11 +66,7 @@ server.post('/getweather', (req, res) => {
       return axios.get(weatherReq);
     }
   }).then((response) => {
-    // res.send({
-    //   address: addr,
-    //   summary: response.data.currently.summary,
-    //   temperature: (response.data.currently.temperature-32)*0.5556,
-    // });
+    
     let temperature = (response.data.currently.temperature-32)*0.5556;
     temperature = temperature.toFixed(2);
     res.render('result.hbs', {
@@ -72,45 +77,13 @@ server.post('/getweather', (req, res) => {
   })
   .catch((error) => {
     console.log('ERROR: ', error.message);
-    res.send(error.message);
+    //res.send(error.message);
+    res.render('error.hbs', {
+      errorMsg: error.message,
+    });
     //res.send(error.message);
   });
 });
-
-
-
-
-
-// server.get('/', (req, res) => {
-//   //const addr = req.params.addr;   /:addr
-//   const addr = req.query.addr; //  /?addr=
-//   console.log(addr);
-//   const locationReq = `https://maps.googleapis.com/maps/api/geocode/json?address=${addr}&key=AIzaSyAn7h3tsW_p0md5iISNFzLcJDoRGRgjWPg`;
-//
-//   axios.get(locationReq).then((response) => {
-//     if (response.data.status === 'ZERO_RESULTS'){
-//       throw new Error('Unable to find that address');
-//       res.send('Unable to find that address');
-//     } else {
-//       const addr = response.data.results[0].formatted_address;
-//       const lat = response.data.results[0].geometry.location.lat;
-//       const lng = response.data.results[0].geometry.location.lng;
-//       const weatherReq = `https://api.darksky.net/forecast/a8657815b96e87afa1e337d1fb2329d8/${lat},${lng}`;
-//       return axios.get(weatherReq);
-//     }
-//   }).then((response) => {
-//     res.send({
-//       address: addr,
-//       summary: response.data.currently.summary,
-//       temperature: (response.data.currently.temperature-32)*0.5556,
-//     });
-//   })
-//   .catch((error) => {
-//     console.log('ERROR: ', error.message);
-//     res.send(error.message);
-//     //res.send(error.message);
-//   });
-// });
 
 server.listen(3000, () => {
   console.log("server started on port 3000");
